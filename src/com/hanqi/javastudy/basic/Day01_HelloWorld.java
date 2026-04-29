07a3c9b2eb65161b1711f3ca57fe6a0cd092b83a
han-qis-treasure <w20050120@outlook.com>
2026-04-29 20:36:56 +0800

    Hello World!

---
+++ src/com/hanqi/javastudy/basic/Day01_HelloWorld.java
@@ -0,0 +1,9 @@
+package com.hanqi.javastudy.basic;
+
+public class Day01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java Day01");
+        System.out.println("I am learning Java");
+    }
+}
