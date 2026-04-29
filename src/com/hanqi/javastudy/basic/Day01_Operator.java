dab25adefe3381f6d9c82421bf042d0c01f415ae
han-qis-treasure <w20050120@outlook.com>
2026-04-30 00:00:56 +0800

    Operator

---
+++ src/com/hanqi/javastudy/basic/Day01_Operator.java
@@ -0,0 +1,23 @@
+package com.hanqi.javastudy.basic;
+
+public class Day01_Operator {
+    public static void main(String[] args) {
+        //整数的运算
+        int a = 10;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println("---------------------");
+        //浮点数的运算
+        double c=10.0;
+        double d=3.0;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
