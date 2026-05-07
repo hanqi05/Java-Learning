92d47433abd54179a97bee253794a7fc624d822e
han-qis-treasure <w20050120@outlook.com>
2026-05-07 18:15:11 +0800

    SwitchPractice

---
+++ src/com/hanqi/learn/day03/SwitchFallThroughPractice.java
@@ -0,0 +1,35 @@
+package com.hanqi.learn.day03;
+
+import java.util.Scanner;
+
+public class SwitchFallThroughPractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个月份");
+        int month = sc.nextInt();
+        switch (month) {
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Invalid month");
+        }
+    }
+}
--- src/com/hanqi/learn/day03/Week.java
+++ src/com/hanqi/learn/day03/Week.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Week {
     public static void main(String[] args) {
-        System.out.println("请输入一个数");
         Scanner sc = new Scanner(System.in);
         int week = sc.nextInt();
         switch (week) {
