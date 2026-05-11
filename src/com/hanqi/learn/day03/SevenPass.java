2bc4439b3a261caca186242a7d8059b28676ef78
han-qis-treasure <w20050120@outlook.com>
2026-05-11 23:14:48 +0800

    SevenPass

---
+++ src/com/hanqi/learn/day03/SevenPass.java
@@ -0,0 +1,13 @@
+package com.hanqi.learn.day03;
+
+public class SevenPass {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 7 == 0||i%10==7||i/10==7){
+                System.out.println("pass");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
