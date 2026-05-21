f1a612ec2c5262f23d228c16127a8971e1833051
han-qis-treasure <w20050120@outlook.com>
2026-05-21 22:51:11 +0800

    Array Practice

--- src/com/hanqi/learn/day04/ArrayMaxMin.java
+++ src/com/hanqi/learn/day04/ArrayMaxMin.java
@@ -7,14 +7,10 @@ public class ArrayMaxMin {
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
            if (arr[i] > max){
-               int temp = max;
                max=arr[i];
-               arr[i] = temp;
            }
            if (arr[i] < min){
-               int temp = min;
                min=arr[i];
-               arr[i] = temp;
            }
         }
         System.out.println("最大值为："+max+" "+"最小值为："+min);
