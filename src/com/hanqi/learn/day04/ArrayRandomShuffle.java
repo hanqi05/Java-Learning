430916749cf873f24907d487fc0a76d479c5cb68
han-qis-treasure <w20050120@outlook.com>
2026-05-22 12:39:00 +0800

    Array Practice

---
+++ src/com/hanqi/learn/day04/ArrayRandomShuffle.java
@@ -0,0 +1,19 @@
+package com.hanqi.learn.day04;
+
+import java.util.Random;
+
+public class ArrayRandomShuffle {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int randomIndex = random.nextInt(arr.length);
+            int temp = arr[i];
+            arr[i] = arr[randomIndex];
+            arr[randomIndex] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
