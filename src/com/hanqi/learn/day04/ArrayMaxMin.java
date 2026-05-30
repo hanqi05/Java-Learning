43badcccb01b7994b3ec0099a62aadd29d616004
han-qis-treasure <w20050120@outlook.com>
2026-05-30 15:11:36 +0800

    Array Practice

--- src/com/hanqi/learn/day04/ArrayMaxMin.java
+++ src/com/hanqi/learn/day04/ArrayMaxMin.java
@@ -2,19 +2,21 @@ package com.hanqi.learn.day04;
 
 public class ArrayMaxMin {
     public static void main(String[] args) {
-        int[] arr = {33,5,22,44,55,1,11};
+        int[] arr = {12, 45, 78, 23, 99, 34};
         int max = arr[0];
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
-           if (arr[i] > max){
-               max=arr[i];
-           }
-           if (arr[i] < min){
-               min=arr[i];
-           }
+            if (arr[i] > max) {
+                int temp = arr[i];
+                arr[i] = max;
+                max = temp;
+            }
+            if (arr[i] < min) {
+                int temp = arr[i];
+                arr[i] = min;
+                min = temp;
+            }
         }
-        System.out.println("最大值为："+max+" "+"最小值为："+min);
-
-
+        System.out.println("最大值为：" + max + " 最小值为：" + min);
     }
 }
