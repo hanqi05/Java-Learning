5c227f319a579453078f0705397c400cab258989
han-qis-treasure <w20050120@outlook.com>
2026-05-31 16:37:04 +0800

    Array Practice

--- src/com/hanqi/learn/day04/ArrayRandomShuffle.java
+++ src/com/hanqi/learn/day04/ArrayRandomShuffle.java
@@ -13,7 +13,7 @@ public class ArrayRandomShuffle {
             arr[randomIndex] = temp;
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 }
